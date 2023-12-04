6ac19d573377e0bad31de09517448de3ba5d5612
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2023-12-04 22:41:12 +0300

    buildable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+
 //, useReducer
 import {Navbar, Container, Button, Row, Col, Nav, Badge, Form} from 'react-bootstrap'
 import axios from "axios";
@@ -6,8 +7,25 @@ import axios from "axios";
 interface UserData {
     user_id: string;
     email: string;
-    projects: object;
+    projects: string[];
 }
+
+interface CurrenProject {
+    projectName: string;
+    names: ProjectNames[];
+}
+
+interface ProjectNames {
+    name: string;
+    namespaces: Namespace[];
+}
+
+interface Namespace {
+    namespace_id: number;
+    result: number;
+}
+
+//type CurrentProjectType <>
 
 /*const formReducer = (state, event) => {
     return {
@@ -19,10 +37,41 @@ interface UserData {
 function App() {
     const [userId, setUserId] = useState<string | null>(null);
     const [userEmail, setUserEmail] = useState<string | null>(null);
-    const [userProjects, setUserProjects] = useState<Array<string> | null>(null);
-    const [currentProject, setCurrentProject] = useState<Array<string> | null>(null);
-    //const [submitting, setSubmitting] = useState(false);
-    //const [formData, setFormData] = useReducer(formReducer, {});
+    /*const [userProjects, setUserProjects] = useState<{
+        name: string,
+        names: []
+    } | null>(null);*/
+    const [currentProject, setCurrentProject] = useState<CurrenProject>({
+        projectName: "My project",
+        names: [
+            /*{
+                name: "example",
+                namespaces: [
+                    {
+                        namespace_id: 1,
+                        result: 1,
+                    },
+                    {
+                        namespace_id: 2,
+                        result: 0,
+                    }
+                ]
+            },
+            {
+                name: "other",
+                namespaces: [
+                    {
+                        namespace_id: 1,
+                        result: 0,
+                    },
+                    {
+                        namespace_id: 2,
+                        result: 0,
+                    }
+                ]
+            }*/
+        ]
+    });
 
     // todo: enum
     const namespaceNames: {[index: number]: string} = {
@@ -37,24 +86,25 @@ function App() {
         8: "github username",
         9: "telegram channel",
         10: "telegram bot",
+        //todo: 11: "etsy",
     };
 
     useEffect(() => {
         // Выполняем запрос к /api/user
-        axios.get<UserData>('/api/user')
+        axios.get<UserData>('http://localhost:37011/api/user')
             .then(response => {
                 const user_id = response.data.user_id;
                 const email = response.data.email;
                 const projects = response.data.projects;
                 setUserId(user_id);
                 setUserEmail(email);
-                setUserProjects(projects);
+                //setUserProjects(projects);
 
                 // Здесь можно вызвать нужные функции на основе полученного user_id
                 if (user_id) {
                     //console.log(projects);
                     const currentProject = Object.keys(projects)[0];
-                    axios.post('/api/load_project', {project_id: currentProject})
+                    axios.post('http://localhost:37011/api/load_project', {project_id: currentProject})
                         .then(response => {
                             //console.log(response.data[0]);
                             setCurrentProject(response.data[0]);
@@ -70,39 +120,38 @@ function App() {
     }, []);
 
     function loginWithGoogle() {
-        window.location.href = '/api/google-oauth-redirect'; // Замените на нужный URL
+        window.location.href = 'http://localhost:37011/api/google-oauth-redirect'; // Замените на нужный URL
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        //setSubmitting(true);
-        console.log(event.target.elements);
-        const tempPlayer = new FormData(event.target);
-        console.log(tempPlayer);
-        axios.post('/api', tempPlayer);
+        const target = event.currentTarget;
 
-        /*setTimeout(() => {
-            setSubmitting(false);
-        }, 3000)*/
+        //setSubmitting(true);
+        axios.post('http://localhost:37011/api/check_name', new FormData(target))
+            .then(response => {
+                if (response.data.success === true && event.target !== undefined) {
+                    const checkedName = target.checkedName.value;
+                    //const checkedName = target.elements.name.value;
+                    setCurrentProject(prevState => ({
+                        ...prevState,
+                        names: [{
+                            name: checkedName,
+                            namespaces: response.data.result,
+                        }, ...prevState.names]
+                    }));
+                } else {
+                    console.log('err');
+                }
+            });
     };
 
-/*    const handleChange = event => {
-        setFormData({
-            name: event.target.name,
-            value: event.target.value,
-        });
-    }*/
-    /*function submitForm(event): void {
-        event.preventDefault();
-        console.log(event.target.elements);
-    }
-*/
     return (
         <div>
             <Navbar className="bg-body-tertiary mb-3">
                 <Container>
-                    <Navbar.Brand>Aboo.ru - check free names for your project</Navbar.Brand>
-                    <div id="userContainer">
+                    <Navbar.Brand>{/*Aboo.ru - */}Check free names for your project</Navbar.Brand>
+                    <div id="userContainer" style={{display: "none"}}>
                         { userId ? <div>Logged in as {userEmail}</div> : <Button variant="outline-primary" onClick={loginWithGoogle}>Sign in via Google</Button> }
                     </div>
                 </Container>
@@ -115,6 +164,7 @@ function App() {
                                 type="text"
                                 name="name"
                                 placeholder="name or username"
+                                id="checkedName"
                                 className="form-control-lg"
 
                             />{/*onChange={handleChange}*/}
@@ -123,10 +173,10 @@ function App() {
                         </Col>
 
                         <Col xs="8">
-                            {Object.keys(namespaceNames).map(function (index: number) {
+                            {Object.keys(namespaceNames).map(function (item, index: number) {
                                 return <div key={index}>
                                     <label htmlFor="{`nametype-${index}`}">
-                                        <input type="checkbox" name="{`namespace-${index}`}" id="{`nametype-${index}`}" value="{ index }" /> {namespaceNames[index]}</label>
+                                        <input type="checkbox" name={`namespaces[${item}]`} id="{`nametype-${index}`}" /> {namespaceNames[index]}</label>
                                 </div>
                             })}
                         </Col>
@@ -134,10 +184,10 @@ function App() {
                 </form>
 
                 <Nav className="nav-tabs mb-3">
-                    {userProjects && Object.keys(userProjects).map(function (projectKey) {
+                    {/*{userProjects && Object.keys(userProjects).map(function (projectKey) {
                         // todo: отметить выбранный классом active
                         return <a key={projectKey} className="nav-link active">{userProjects[projectKey]}</a>
-                    })}
+                    })}*/}
                 </Nav>
 
                 <ul className="list-group list-group-flush">
